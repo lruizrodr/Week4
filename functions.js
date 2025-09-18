b20bab13f94a1353be92f0c1bbb0e2ecca405a7c
lruizrodr <lruiz0002@gmail.com>
2025-09-18 14:01:44 -0500

    thursday morning section

--- functions.js
+++ functions.js
@@ -83,11 +83,11 @@
 
 // SIMPLE FUNCTION:
 
-function greet(){
-    console.log("Hello to JS functions!");
-    }
+// function greet(){
+//     console.log("Hello to JS functions!");
+//     }
 
-greet();
+// greet();
 
 //function with parameters:
 
@@ -177,23 +177,173 @@ greet();
 
 //practice:
 
-function runprogram(){
-    let lightsabers = ["blue", "green", "red", "yellow"];
-    let choice;
+// function runprogram(){
+//     let lightsabers = ["blue", "green", "red", "yellow"];
+//     let choice;
 
-    while(true){
-        choice = prompt("Turn on: Blue, Green, Red, Yellow, or quit");
-        if(choice.toLowerCase() === "quit"){
-            console.log("Goodbye!");
-            break; // stops the loop
-        }
-        if(lightsabers.includes(choice.toLowerCase())){
-            console.log(choice + " lightsaber turned on.");
-        }
-        else{
-            console.log(choice + " is not a valid option.");
-        }
-    }
-}
+//     while(true){
+//         choice = prompt("Turn on: Blue, Green, Red, Yellow, or quit");
+//         if(choice.toLowerCase() === "quit"){
+//             console.log("Goodbye!");
+//             break; // stops the loop
+//         }
+//         if(lightsabers.includes(choice.toLowerCase())){
+//             console.log(choice + " lightsaber turned on.");
+//         }
+//         else{
+//             console.log(choice + " is not a valid option.");
+//         }
+//     }
+// }
 
-runprogram();
+// runprogram();
+
+// let person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30,
+//     greet: function(){
+//         console.log("Hello" + this.firstName);
+//     },
+//     work: function(){
+//         console.log(this.firstName + " is working...");
+//     }
+// };
+
+// let car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: "2018",
+//     customeCleared: false,
+//     start: function(){
+//         console.log("Car started...");
+//     },
+//     stop: function(){
+//         console.log("Car stopped...");
+//     }
+// };
+// let a_ = car.brand;
+// let b_ = car.model;
+
+// let {brand, model} = car;
+// console.log(brand);
+// console.log(model);
+
+// console.log(car);
+// console.log(car.brand);
+// console.log(car.year);
+
+// car.start();
+// car.stop();
+
+// let starfighter = {
+//     model: "T-65B",
+//     propulsion: "4 x Incom 4L4 fusial thrust engines",
+//     hyperdrive: "Class-1 Koensayr GBk-585",
+//     systems: "Novaldex 04-z cryogenic power generator",
+//     customeCleared: false,
+//     start: function(){
+//         console.log("Starfighter started...");
+//     },
+//     stop: function(){
+//         console.log("Starfighter stopped...");
+//     }
+// };
+
+// console.log(starfighter);
+// console.log(starfighter.model);
+// console.log(starfighter.propulsion);
+// console.log(starfighter.hyperdrive);
+// console.log(starfighter.systems);
+
+// starfighter.start();
+// starfighter.stop();
+
+
+// Destructing
+// let numbers = [10, 20, 30, 40, 50];
+
+// let [a, b, c, d] = numbers;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// // spread operator (...)
+
+// let veggies = ["apple", "banana", "mango"];
+// let fruits = ["orange", "grape", "pear"];
+
+// let food = [...veggies,...fruits];
+
+// console.log(food);
+
+// let copyFood = [...food];
+// copyFood = "cherry";
+
+// console.log(food);
+
+// let user = {name: "John Doe", age: 30};
+// let details = { city: "New York", country: "USA"};
+// let userProfile = {...user,...details};
+
+// console.log(userProfile);
+
+// let person1 = {name: "Robert"};
+// let person2 = {name: "Luis"};
+
+// person2 = person1;
+
+// person2.name = "Jake";
+
+// console.log(person1);
+// console.log(person2);
+
+// DEEP CLONING
+
+// person2 = structuredClone(person1);
+// person2.name = "Jake";
+
+// console.log(person1.name);
+// console.log(person2.name);
+
+//Write a function that accepts an array(list of letters) as parameter, the function should display the count of each item that is in the array. 
+//you have to pass the array values to the function from prompt. 
+//how to pass array values into the functions:
+//when you start the browser, a prompt should be there asking you to enter the item, you will keep adding the items in the array until you pass 0 to exit. 
+
+//the output should be: 
+//a appears 3 times
+//b appears 2 times
+//c appears 3 times
+
+// function countItems(arr){
+//     let countObj = {};
+//     for (let i = 0; i < arr.length; i++){
+//         let item = arr[i];
+//         if (countObj[item]) {
+//             countObj[item] = countObj[item] + 1;
+//         } else {
+//             countObj[item] = 1;
+//         }
+//     }
+//     for (let key in countObj){
+//         console.log(key + " appears " + countObj[key] + " times");
+//     }
+// }
+// let items = [];
+// while (true) {
+//     let input = prompt("Enter letters separated by comma (type 0 to stop):"); 
+//     if (input === "0"){
+//         break;
+//     }
+//     let letters = input.split(",");
+//     for (let i = 0; i < letters.length; i++){
+//         let letter = letters[i].trim().toLowerCase(); 
+//         if (letter !== "") {
+//             items.push(letter);
+//         }
+//     }
+// }
+// countItems(items);
